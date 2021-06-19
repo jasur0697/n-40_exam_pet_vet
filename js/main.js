600ec2e1c32fdb93abf6548eec0b64c49f8f6005
JasurbekNurmamatov <63550009+JasurbekNurmamatov@users.noreply.github.com>
2021-06-19 00:04:16 -0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,39 @@
+let elsTabToggle = document.querySelectorAll('.tab__toggle');
+let elTab = document.querySelector('.tab');
+
+elsTabToggle.forEach(toggle => {
+  toggle.addEventListener('click', () => {
+    toggle.closest('.tab').classList.toggle('tab__open')
+  })
+})
+
+
+
+
+
+// var elsFeatItem = document.querySelectorAll('.features__item');
+// var elsFeatBtn = document.querySelectorAll('.features__button');
+
+// elsFeatBtn.forEach(function (link) {
+//   link.addEventListener('click', function () {
+//     elsFeatItem.forEach(function (item) {
+//       item.classList.remove('features__button-active');
+//     });
+
+//     link.parentElement.classList.add('features__button-active');
+//   });
+// });
+
+
+
+
+// var elsQuestionsItem = document.querySelectorAll('.questions__item');
+// var elsQuestionsButton = document.querySelectorAll('.questions__item--button');
+// elsQuestionsButton.forEach(function (button) {
+//   button.addEventListener('click', function () {
+//     elsQuestionsItem.forEach(function (item) {
+//       item.classList.remove('questions__item--active');
+//     });
+//     button.closest('.questions__item').classList.add('questions__item--active');
+//   });
+// });
